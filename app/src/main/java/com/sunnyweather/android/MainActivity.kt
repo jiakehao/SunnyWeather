445599c2831c655a0219fad8accce217a779470a
Jiake Hao <scyjh1@nottingham.edu.cn>
2021-02-01 23:39:18 +0800

    加入显示天气信息的功能

--- app/src/main/java/com/sunnyweather/android/MainActivity.kt
+++ app/src/main/java/com/sunnyweather/android/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.sunnyweather.android
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.sunnyweather.android.logic.model.Sky
+import com.sunnyweather.android.logic.model.getSky
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
